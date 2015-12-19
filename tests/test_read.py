4a9862e02560041eb2ee3b8fab18e8fa143780e8
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2015-12-19 12:54:17 -0800

    new tests
    test if extensions parsed correctly

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,27 +1,60 @@
-__author__ = 'Yann-Sebastien'
-
 import unittest
-import os
+import tempfile
 
 import nanonispy as nap
 
 class TestNanonisFileBaseClass(unittest.TestCase):
-    def setUp(self, ext='3ds'):
-        self.test_filename = 'tests_nanonis_file.' + ext
-        f = open(self.test_filename, 'w')
-        f.close()
-
-    def tearDown(self):
-        os.remove(self.test_filename)
+    """
+    Testing class for NanonisFile base class.
+    """
 
     def test_is_instance_nanonis_file(self):
-        self.NF = nap.read.NanonisFile(self.test_filename)
-        self.assertIsInstance(self.NF, nap.read.NanonisFile)
+        """
+        Check for correct instance of NanonisFile object.
+        """
+        with tempfile.NamedTemporaryFile(suffix='.3ds') as f:
+            NF = nap.read.NanonisFile(f.name)
+        self.assertIsInstance(NF, nap.read.NanonisFile)
 
     def test_unsupported_filetype(self):
-        exception = nap.read.UnhandledFileError
-        with self.assertRaises(exception):
-            self.NF = nap.read.NanonisFile('not_a_file.txt')
+        """
+        Handle unsupported file gracefully.
+        """
+        with self.assertRaises(nap.read.UnhandledFileError):
+            with tempfile.NamedTemporaryFile(suffix='.txt') as f:
+                NF = nap.read.NanonisFile(f.name)
+
+    def test_3ds_suffix_parsed(self):
+        """
+        3ds file recognized.
+        """
+        with tempfile.NamedTemporaryFile(suffix='.3ds') as f:
+            NF = nap.read.NanonisFile(f.name)
+            self.assertEquals(NF.filetype, '3ds')
+
+    def test_sxm_suffix_parsed(self):
+        """
+        Sxm file recognized.
+        """
+        with tempfile.NamedTemporaryFile(suffix='.sxm') as f:
+            NF = nap.read.NanonisFile(f.name)
+            self.assertEquals(NF.filetype, 'sxm')
+
+    def test_dat_suffix_parsed(self):
+        """
+        Dat file recognized.
+        """
+        with tempfile.NamedTemporaryFile(suffix='.dat') as f:
+            NF = nap.read.NanonisFile(f.name)
+            self.assertEquals(NF.filetype, 'dat')
+
+    def test_header_found(self):
+        with tempfile.NamedTemporaryFile(suffix='.3ds') as f:
+            f.write(r'header_entry\nend_tag\n')
+            f.seek(0)
+            NF = nap.read.NanonisFile(f.name)
+            self.assertEquals(NF.header, 'header_entry')
+
 
 
 if __name__ == '__main__':
