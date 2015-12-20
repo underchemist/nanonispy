30d3a4902bed0856a47c58b8e50d4698601c8351
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2015-12-20 00:10:46 -0800

    Somewhat less stupid tests, skeleton subclasses
    Found a workaround for issue with simply creating tempfile and trying to
    initialize NanonisFile from it, since there was still an io buffer
    object opened on whatever temporary filename.
    
    Now I create a temp dir and don't delete temp files within and then
    cleanup the entire directory in the tearDown() method.
    
    Some skeleton classes for the three subclasses were added as well for a
    signal class.

--- read.py
+++ read.py
@@ -6,17 +6,85 @@ class NanonisFile:
     """
 
     def __init__(self, fname):
-        self.datadir, self.fname = os.path.split(fname)
+        self.datadir, self.basename = os.path.split(fname)
+        self.fname = fname
         self.filetype = self._determine_filetype()
+        self.end_tags = dict(grid=':HEADER_END:',
+                             scan='SCANIT_END',
+                             spec='[DATA]')
+
+        self.byte_offset = self.find_data_start_byte()
+        self.header_raw = self.gen_raw_header(self.byte_offset)
 
     def _determine_filetype(self):
-        if self.fname[-3:] not in ('3ds', 'sxm', 'dat'):
-            raise UnhandledFileError('{} is not a supported filetype or does not exist'.format(self.fname))
+        """
+        Check last three characters for appropriate file extension,
+        raise error if not.
+        """
+
+        if self.fname[-3:] == '3ds':
+            return 'grid'
+        elif self.fname[-3:] == 'sxm':
+            return 'scan'
+        elif self.fname[-3:] == 'dat':
+            return 'spec'
         else:
-            return self.fname[-3:]
+            raise UnhandledFileError('{} is not a supported filetype or does not exist'.format(self.basename))
+
+    def gen_raw_header(self, byte_offset):
+        """
+        Return header as a raw string. Everything before
+        the end tag is considered to be part of the header.
+        the parsing will be done by each subclass of file.
+        """
+        with open(self.fname, 'rb') as f:
+            return f.read(byte_offset)
+
+    def find_data_start_byte(self):
+        """
+        Find first byte after end tag signalling end of header info.
+        """
+        with open(self.fname, 'rb') as f:
+            tag = self.end_tags[self.filetype]
+
+            # Set to a default value to know if end_tag wasn't found
+            byte_offset = -1
+
+            for line in f:
+                # Convert from bytes to str
+                entry = line.strip().decode()
+                if tag in entry:
+                    byte_offset = f.tell()
+                    break
+
+            if byte_offset == -1:
+                raise FileHeaderNotFoundError(
+                        'Could not find the {} end tag in {}'.format(tag, self.basename)
+                        )
+
+        return byte_offset
+
+class Grid(NanonisFile):
+    pass
+
+class Scan(NanonisFile):
+    pass
+
+class Spec(NanonisFile):
+    pass
+
+class Signal:
+    pass
+
 
 class UnhandledFileError(Exception):
     """
     To be raised when unknown file extension is passed.
+    """
+    pass
+
+class FileHeaderNotFoundError(Exception):
+    """
+    To be raised when no header information could be determined.
     """
     pass
--- tests/test_read.py
+++ tests/test_read.py
@@ -1,5 +1,6 @@
 import unittest
 import tempfile
+import os
 
 import nanonispy as nap
 
@@ -7,54 +8,90 @@ class TestNanonisFileBaseClass(unittest.TestCase):
     """
     Testing class for NanonisFile base class.
     """
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.temp_dir.cleanup()
 
     def test_is_instance_nanonis_file(self):
         """
         Check for correct instance of NanonisFile object.
         """
-        with tempfile.NamedTemporaryFile(suffix='.3ds') as f:
-            NF = nap.read.NanonisFile(f.name)
+        f = tempfile.NamedTemporaryFile(mode='wb',
+                                        suffix='.3ds',
+                                        dir=self.temp_dir.name,
+                                        delete=False)
+        f.write(b':HEADER_END:')
+        f.close()
+        NF = nap.read.NanonisFile(f.name)
+
         self.assertIsInstance(NF, nap.read.NanonisFile)
+
 
     def test_unsupported_filetype(self):
         """
         Handle unsupported file gracefully.
         """
         with self.assertRaises(nap.read.UnhandledFileError):
-            with tempfile.NamedTemporaryFile(suffix='.txt') as f:
-                NF = nap.read.NanonisFile(f.name)
+            f = tempfile.NamedTemporaryFile(mode='wb',
+                                        suffix='.txt',
+                                        dir=self.temp_dir.name,
+                                        delete=False)
+            f.close()
+            NF = nap.read.NanonisFile(f.name)
 
     def test_3ds_suffix_parsed(self):
         """
         3ds file recognized.
         """
-        with tempfile.NamedTemporaryFile(suffix='.3ds') as f:
-            NF = nap.read.NanonisFile(f.name)
-            self.assertEquals(NF.filetype, '3ds')
+        f = tempfile.NamedTemporaryFile(mode='wb',
+                                        suffix='.3ds',
+                                        dir=self.temp_dir.name,
+                                        delete=False)
+        f.write(b':HEADER_END:')
+        f.close()
+        NF = nap.read.NanonisFile(f.name)
+        self.assertEquals(NF.filetype, 'grid')
 
     def test_sxm_suffix_parsed(self):
         """
         Sxm file recognized.
         """
-        with tempfile.NamedTemporaryFile(suffix='.sxm') as f:
-            NF = nap.read.NanonisFile(f.name)
-            self.assertEquals(NF.filetype, 'sxm')
+        f = tempfile.NamedTemporaryFile(mode='wb',
+                                        suffix='.sxm',
+                                        dir=self.temp_dir.name,
+                                        delete=False)
+        f.write(b'SCANIT_END')
+        f.close()
+        NF = nap.read.NanonisFile(f.name)
+        self.assertEquals(NF.filetype, 'scan')
 
     def test_dat_suffix_parsed(self):
         """
         Dat file recognized.
         """
-        with tempfile.NamedTemporaryFile(suffix='.dat') as f:
-            NF = nap.read.NanonisFile(f.name)
-            self.assertEquals(NF.filetype, 'dat')
+        f = tempfile.NamedTemporaryFile(mode='wb',
+                                        suffix='.dat',
+                                        dir=self.temp_dir.name,
+                                        delete=False)
+        f.write(b'[DATA]')
+        f.close()
+        NF = nap.read.NanonisFile(f.name)
+        self.assertEquals(NF.filetype, 'spec')
 
-    def test_header_found(self):
-        with tempfile.NamedTemporaryFile(suffix='.3ds') as f:
-            f.write(r'header_entry\nend_tag\n')
-            f.seek(0)
-            NF = nap.read.NanonisFile(f.name)
-            self.assertEquals(NF.header, 'header_entry')
+    def test_find_start_byte(self):
+        f = tempfile.NamedTemporaryFile(mode='wb',
+                                        suffix='.3ds',
+                                        dir=self.temp_dir.name,
+                                        delete=False)
+        f.write(b'header_entry\n:HEADER_END:\n')
+        f.close()
 
+        NF = nap.read.NanonisFile(f.name)
+        byte_offset = NF.find_data_start_byte()
+
+        self.assertEquals(byte_offset, 26)
 
 
 if __name__ == '__main__':
