91b2f8c199f1b578eed286364e19308be3688506
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2015-12-20 18:30:18 -0800

    added Grid class, flushed out more tests.

--- read.py
+++ read.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 class NanonisFile:
     """
@@ -8,15 +9,15 @@ class NanonisFile:
     def __init__(self, fname):
         self.datadir, self.basename = os.path.split(fname)
         self.fname = fname
-        self.filetype = self._determine_filetype()
+        self.filetype = self.determine_filetype()
         self.end_tags = dict(grid=':HEADER_END:',
                              scan='SCANIT_END',
                              spec='[DATA]')
 
-        self.byte_offset = self.find_data_start_byte()
-        self.header_raw = self.gen_raw_header(self.byte_offset)
+        self.byte_offset = self.start_byte()
+        self.header_raw = self.read_raw_header(self.byte_offset)
 
-    def _determine_filetype(self):
+    def determine_filetype(self):
         """
         Check last three characters for appropriate file extension,
         raise error if not.
@@ -31,18 +32,23 @@ class NanonisFile:
         else:
             raise UnhandledFileError('{} is not a supported filetype or does not exist'.format(self.basename))
 
-    def gen_raw_header(self, byte_offset):
+    def read_raw_header(self, byte_offset):
         """
-        Return header as a raw string. Everything before
-        the end tag is considered to be part of the header.
-        the parsing will be done by each subclass of file.
+        Return header as a raw string.
+
+        Everything before the end tag is considered to be part of the header.
+        the parsing will be done later by subclass methods.
         """
         with open(self.fname, 'rb') as f:
-            return f.read(byte_offset)
+            return f.read(byte_offset).decode()
 
-    def find_data_start_byte(self):
+    def start_byte(self):
         """
         Find first byte after end tag signalling end of header info.
+
+        Caveat, I believe this is the first byte after the end of the
+        line that the end tag is found on, not strictly the first byte
+        directly after the end tag is found.
         """
         with open(self.fname, 'rb') as f:
             tag = self.end_tags[self.filetype]
@@ -65,7 +71,45 @@ class NanonisFile:
         return byte_offset
 
 class Grid(NanonisFile):
-    pass
+    def __init__(self, fname):
+        super().__init__(fname)
+        self.header = _parse_3ds_header(self.header_raw)
+        self.signals = self._load_data()
+
+    def _load_data(self):
+        # load grid params
+        nx, ny = self.header['dim_px']
+        num_sweep = self.header['num_sweep_signal']
+        num_param = self.header['num_parameters']
+        num_chan = self.header['num_channels']
+        data_dict = dict()
+
+        # open and seek to start of data
+        f = open(self.fname, 'rb')
+        f.seek(self.byte_offset)
+        data_format = '>f4'
+        griddata = np.fromfile(f, dtype=data_format)
+        f.close()
+
+        # pixel size in bytes
+        exp_size_per_pix = num_param + num_sweep*num_chan
+
+        # reshape from 1d to 3d
+        griddata_shaped = griddata.reshape((nx, ny, exp_size_per_pix))
+
+        # experimental parameters are first num_param of every pixel
+        params = griddata_shaped[:, :, :num_param]
+        data_dict['params'] = params
+
+        # extract data for each channel
+        for i, chann in enumerate(self.header['channels']):
+            start_ind = num_param + i * num_sweep
+            stop_ind = num_param + (i+1) * num_sweep
+            data_dict[chann] = griddata_shaped[:, :, start_ind:stop_ind]
+
+
+        return data_dict
+
 
 class Scan(NanonisFile):
     pass
@@ -88,3 +132,76 @@ class FileHeaderNotFoundError(Exception):
     To be raised when no header information could be determined.
     """
     pass
+
+def _parse_3ds_header(header_raw):
+    """
+    Parse raw header string.
+
+    Empirically done based on Nanonis header structure. Details can be seen in
+    Nanonis help documentation.
+
+    Parameters
+    ----------
+    header_raw : str
+        Raw header string from read_raw_header() method.
+    """
+    # cleanup string and remove end tag as entry
+    header_entries = header_raw.split('\r\n')
+    header_entries = header_entries[:-2]
+
+    header_dict = dict()
+
+    # grid dimensions in pixels
+    dim_px_str = _split_header_entry(header_entries[0])
+    header_dict['dim_px'] = [int(val) for val in dim_px_str.split(' x ')]
+
+    # grid frame center position, size, angle
+    grid_str = _split_header_entry(header_entries[1], multiple=True)
+    header_dict['pos_xy'] = [float(val) for val in grid_str[:2]]
+    header_dict['size_xy'] = [float(val) for val in grid_str[2:4]]
+    header_dict['angle'] = float(grid_str[-1])
+
+    # sweep signal
+    header_dict['sweep_signal'] = _split_header_entry(header_entries[2])
+
+    # fixed parameters
+    header_dict['fixed_parameters'] = _split_header_entry(header_entries[3], multiple=True)
+
+    # experimental parameters
+    header_dict['experimental_parameters'] = _split_header_entry(header_entries[4], multiple=True)
+
+    # number of parameters (each 4 bytes)
+    header_dict['num_parameters'] = int(_split_header_entry(header_entries[5]))
+
+    # experiment size in bytes
+    header_dict['experiment_size'] = int(_split_header_entry(header_entries[6]))
+
+    # number of points of sweep signal
+    header_dict['num_sweep_signal'] = int(_split_header_entry(header_entries[7]))
+
+    # channel names
+    header_dict['channels'] = _split_header_entry(header_entries[8], multiple=True)
+    header_dict['num_channels'] = len(header_dict['channels'])
+
+    # measure delay
+    header_dict['measure_delay'] = float(_split_header_entry(header_entries[9]))
+
+    # metadata
+    header_dict['experiment_name'] = _split_header_entry(header_entries[10])
+    header_dict['start_time'] = _split_header_entry(header_entries[11])
+    header_dict['end_time'] = _split_header_entry(header_entries[12])
+    header_dict['user'] = _split_header_entry(header_entries[13])
+    header_dict['comment'] = _split_header_entry(header_entries[14])
+
+    return header_dict
+
+
+def _split_header_entry(entry, multiple=False):
+
+        _, val_str = entry.split("=")
+
+        if multiple:
+            return val_str.strip('"').split(';')
+        else:
+            return val_str.strip('"')
+
--- tests/test_read.py
+++ tests/test_read.py
@@ -1,6 +1,7 @@
 import unittest
 import tempfile
 import os
+import numpy as np
 
 import nanonispy as nap
 
@@ -89,7 +90,7 @@ class TestNanonisFileBaseClass(unittest.TestCase):
         f.close()
 
         NF = nap.read.NanonisFile(f.name)
-        byte_offset = NF.find_data_start_byte()
+        byte_offset = NF.start_byte()
 
         self.assertEquals(byte_offset, 26)
 
@@ -102,6 +103,79 @@ class TestNanonisFileBaseClass(unittest.TestCase):
             f.close()
             NF = nap.read.NanonisFile(f.name)
 
+    def test_header_raw_is_str(self):
+        f = tempfile.NamedTemporaryFile(mode='wb',
+                                        suffix='.3ds',
+                                        dir=self.temp_dir.name,
+                                        delete=False)
+        f.write(b'header_entry\n:HEADER_END:\n')
+        f.close()
+
+        NF = nap.read.NanonisFile(f.name)
+        self.assertIsInstance(NF.header_raw, str)
+
+
+class TestGridFile(unittest.TestCase):
+
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.temp_dir.cleanup()
+
+    def create_dummy_header(self):
+        """
+        return tempfile file object with dummy header info
+        """
+        f = tempfile.NamedTemporaryFile(mode='wb',
+                                        suffix='.3ds',
+                                        dir=self.temp_dir.name,
+                                        delete=False)
+        f.write(b'Grid dim="230 x 230"\r\nGrid settings=4.026839E-8;-4.295725E-8;1.500000E-7;1.500000E-7;0.000000E+0\r\nSweep Signal="Bias (V)"\r\nFixed parameters="Sweep Start;Sweep End"\r\nExperiment parameters="X (m);Y (m);Z (m);Z offset (m);Settling time (s);Integration time (s);Z-Ctrl hold;Final Z (m)"\r\n# Parameters (4 byte)=10\r\nExperiment size (bytes)=2048\r\nPoints=512\r\nChannels="Input 3 (A)"\r\nDelay before measuring (s)=0.000000E+0\r\nExperiment="Grid Spectroscopy"\r\nStart time="21.10.2014 16:48:06"\r\nEnd time="23.10.2014 10:42:19"\r\nUser=\r\nComment=\r\n:HEADER_END:\r\n')
+        f.close()
+
+        return f
+
+    def create_dummy_data(self):
+        """
+        return tempfile file object with dummy header info
+        """
+        f = tempfile.NamedTemporaryFile(mode='wb',
+                                        suffix='.3ds',
+                                        dir=self.temp_dir.name,
+                                        delete=False)
+        f.write(b'Grid dim="230 x 230"\r\nGrid settings=4.026839E-8;-4.295725E-8;1.500000E-7;1.500000E-7;0.000000E+0\r\nSweep Signal="Bias (V)"\r\nFixed parameters="Sweep Start;Sweep End"\r\nExperiment parameters="X (m);Y (m);Z (m);Z offset (m);Settling time (s);Integration time (s);Z-Ctrl hold;Final Z (m)"\r\n# Parameters (4 byte)=10\r\nExperiment size (bytes)=2048\r\nPoints=512\r\nChannels="Input 3 (A)"\r\nDelay before measuring (s)=0.000000E+0\r\nExperiment="Grid Spectroscopy"\r\nStart time="21.10.2014 16:48:06"\r\nEnd time="23.10.2014 10:42:19"\r\nUser=\r\nComment=\r\n:HEADER_END:\r\n')
+        a = np.linspace(0, 100.0, 230*230*(10+512))
+        b = np.asarray(a, dtype='>f4')
+        b.tofile(f)
+        f.close()
+
+        return f
+
+    def test_is_instance_grid_file(self):
+        """
+        Check for correct instance of Grid object.
+        """
+        f = self.create_dummy_data()
+        GF = nap.read.Grid(f.name)
+
+        self.assertIsInstance(GF, nap.read.Grid)
+
+    def test_header_is_dict(self):
+        f = self.create_dummy_data()
+        GF = nap.read.Grid(f.name)
+
+        self.assertIsInstance(GF.header, dict)
+
+    def test_data_has_right_shape(self):
+        f = self.create_dummy_data()
+        GF = nap.read.Grid(f.name)
+
+        self.assertEquals(GF.signals['Input 3 (A)'].shape, (230, 230, 512))
+
+
+class TestUtilityFunctions(unittest.TestCase):
+    pass
 
 if __name__ == '__main__':
     unittest.main()
