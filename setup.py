8e3ef0956c6b97ae3d7f70d06c04b95451292604
Yann-Sebastien <yanns.tremblay@gmail.com>
2015-12-25 21:30:07 -0800

    removed download_url

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
+from setuptools import setup
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md')) as f:
     long_description = f.read()
 
 setup(
@@ -25,7 +22,7 @@ setup(
     url='https://github.com/underchemist/nanonispy',
 
     # Download url
-    download_url = 'https://github.com/underchemist/nanonispy/tarball/master',
+    # download_url = 'https://github.com/underchemist/nanonispy/tarball/master',
 
     # Author details
     author='Yann-Sebastien Tremblay-Johnston',
@@ -44,7 +41,7 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: STM :: Nanonis',
+        'Topic :: Scientific/Engineering :: Physics',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
@@ -60,45 +57,18 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['nanonispy'],
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'test': ['nose', 'coverage']
-    },
+    packages=['nanonispy', 'nanonispy.tests'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'test_spec_data': ['Bias-Spectrosocpy002.dat'],
+        'nanonispy': ['LICENSE', 'README.md'],
     },
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
+    install_requires=[
+          'numpy',
+      ],
 
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
+    tests_require = ['nose', 'coverage'],
 )
