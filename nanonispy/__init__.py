d614e00ad396970dc65a99c7081513dee70389f8
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2017-06-29 00:34:46 -0700

    continued tinkering, includes utils module in init.py now

--- nanonispy/__init__.py
+++ nanonispy/__init__.py
@@ -1,1 +1,2 @@
 from . import read
+from . import utils
--- nanonispy/utils.py
+++ nanonispy/utils.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 import numpy as np
 
-def show_grid(arr):
+def show_grid(arr, sweep_signal):
     """
     Plot 2 out of 3 dimensions of a Grid as an image with a slider to move along third dimension
     
@@ -20,17 +20,18 @@ def show_grid(arr):
     emax = arr.shape[-1] -1 
 
     fig, ax = plt.subplots()
-    fig.tight_layout()
-
     ax_energy_ind = fig.add_axes([0.25, 0.10, 0.65, 0.03])
-    s_energy_ind = Slider(ax_energy_ind, 'Energy index', emin, emax, valinit=default_energy_index, valfmt='%i')
+    s_energy_ind = Slider(ax_energy_ind, 'Energy index', emin, emax, valinit=default_energy_index)
 
     im = ax.imshow(arr[:, :, s_energy_ind.val])
 
     def update(val):
         im.set_data(arr[:, :, int(s_energy_ind.val)])
         im.autoscale()
-        fig.canvas.draw()
+
+        s_energy_ind.valtext.set_text('{:.2f}'.format(sweep_signal[int(s_energy_ind.val)] * 1000))
 
     s_energy_ind.on_changed(update)
     plt.show()
+
+    return fig, ax, im , s_energy_ind
