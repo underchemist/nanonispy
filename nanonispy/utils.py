0554edd9237547d4bc97eadd32ad567884c04d6a
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2017-07-01 18:23:55 -0700

    cleaning up but still figuring out how to implement this

--- nanonispy/utils.py
+++ nanonispy/utils.py
@@ -10,20 +10,26 @@ def show_grid(arr, sweep_signal):
     ----------
     arr : array_like
         A 3d array consisting of (Ix, Iy, E) or (qx, qy, E) data.
+    sweep_signal : array_like
+        A 1d array consiting of the bias values or other relevant spectroscopic sweep parameter.
+        For example can be passed from Grid.signals['sweep_signal'].
+
 
     Returns
     -------
     ?
     """
+    # starting + min/max values for slider
     default_energy_index = arr.shape[-1] // 2
-    emin = 0
-    emax = arr.shape[-1] -1 
+    energy_min = 0
+    energy_max = arr.shape[-1] -1 
 
     fig, ax = plt.subplots()
-    ax_energy_ind = fig.add_axes([0.25, 0.10, 0.65, 0.03])
-    s_energy_ind = Slider(ax_energy_ind, 'Energy index', emin, emax, valinit=default_energy_index)
+    ax.set_position([0.125, 0.175, 0.80, 0.80])
+    s_ax = fig.add_axes([0.2, 0.10, 0.65, 0.03]) # axis for slider
+    s_energy_ind = Slider(s_ax, 'Energy', energy_min, energy_max, valinit=default_energy_index)
 
-    im = ax.imshow(arr[:, :, s_energy_ind.val])
+    im = ax.imshow(arr[:, :, int(s_energy_ind.val)])
     s_energy_ind.valtext.set_text('{:.2f} mV'.format(sweep_signal[int(s_energy_ind.val)] * 1000))
 
     def update(val):
@@ -35,4 +41,4 @@ def show_grid(arr, sweep_signal):
     s_energy_ind.on_changed(update)
     plt.show()
 
-    return fig, ax, im , s_energy_ind
+    return fig, ax, s_ax, im, s_energy_ind
