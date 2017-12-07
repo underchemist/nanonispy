7b46ed8275fca8669eb031c755206fa0ed33608d
sebastien tremblay <yanns.tremblay@gmail.com>
2017-12-06 23:00:28 -0800

    update setup.py for pypi update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 with open(path.join(here, 'README.rst')) as f:
     long_description = f.read()
-version = '1.0.4'
+version = '1.0.5'
 
 setup(
     name='nanonispy',
@@ -14,7 +14,6 @@ setup(
     description='Library to parse Nanonis files.',
     long_description=long_description,
     url='https://github.com/underchemist/nanonispy',
-    download_url = 'https://github.com/underchemist/nanonispy/tarball/'+version,
     author='Yann-Sebastien Tremblay-Johnston',
     author_email='yanns.tremblay@gmail.com',
     license='MIT',
@@ -24,12 +23,12 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Physics',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
     keywords='science numpy parse library',
+    python_requires='>=3.3',
     packages=['nanonispy', 'nanonispy.tests'],
     package_data={'nanonispy': ['LICENSE', 'README.md'], },
     install_requires=['numpy', ],
